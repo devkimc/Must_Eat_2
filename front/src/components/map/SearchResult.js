8006c343a54f0ae9ad778148faa63cabacb41ea8
franco <kws60000@naver.com>
2022-05-05 22:17:21 +0900

    [MOD] useResetRecoilState 사용하여 데이터 초기화

--- front/src/components/map/SearchResult.js
+++ front/src/components/map/SearchResult.js
@@ -4,23 +4,26 @@ const SearchResult = ({ searchRes }) => {
   return (
     <Container>
       <ul>
-        {searchRes.map(res => (
-          <SearchResultList id={res.id}>{res.place_name}</SearchResultList>
-        ))}
+        {searchRes.length !== 0 ? searchRes.map(res => (
+          <SearchResultList key={res.id}>
+            {res.place_name}
+          </SearchResultList>
+        )) : <div></div>}
       </ul>
     </Container>
   )
 }
 
 const Container = styled.div`
+  height: 100vh;
   overflow: hidden;
   overflow-y: auto;
   flex: 1;
 `
 
-
 const SearchResultList = styled.li`
   padding: 18px 25px 20px;
+  border-top: 1px solid #eee;
 `
 
 export default SearchResult
--- front/src/containers/map/SearchContainer.js
+++ front/src/containers/map/SearchContainer.js
@@ -1,6 +1,6 @@
 import { SearchInput, SearchResult } from 'components'
 
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useResetRecoilState } from 'recoil'
 import { searchIpState, searchResState } from 'recoil/atom/map'
 
 import styled from 'styled-components'
@@ -8,10 +8,11 @@ import styled from 'styled-components'
 const SearchContainer = () => {
   const [searchIp] = useRecoilState(searchIpState)
   const [searchRes, setSearchRes] = useRecoilState(searchResState)
-
+  const resetRes = useResetRecoilState(searchResState)
+  
   /* global kakao */
   const keywordSearch = () => {
-   
+
     const searchOption = {
       category_group_code: 'FD6',      
       x: 126.86483931801229,
@@ -25,14 +26,15 @@ const SearchContainer = () => {
   const keywordSearchCallBack = (res, status) => {
     const resStatus = kakao.maps.services.Status
     if (status === resStatus.OK) {
-      setSearchRes(() => res)
+      setSearchRes(res)
     } else if (status === resStatus.ZERO_RESULT) {
       alert('검색 결과가 없습니다!')
       console.log(status)
-      setSearchRes(() => [])
+      resetRes()
     } else {
       alert('서버 응답에 문제가 있습니다!')
-      setSearchRes(() => [])
+      console.log(status)
+      resetRes()
     }
   }
 
@@ -51,7 +53,7 @@ const Container = styled.div`
   position: absolute;
   z-index: 20;
   width: 390px;
-  height: 100%;
+  height: 100vh;
   box-shadow: 0 0 5px 0 rgb(0 0 0 / 20%), 5px 0 15px 0 rgb(0 0 0 / 10%);
 `
 
