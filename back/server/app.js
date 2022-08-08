1889764b57b676af0b9e049884a6cf9ba4c7e97f
franco <kws60000@naver.com>
2022-08-08 15:54:31 +0900

    refactor: airbnb convention 적용

--- back/server/app.js
+++ back/server/app.js
@@ -2,63 +2,64 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import createError from 'http-errors'
-import cors from 'cors'
-import { createPool } from 'mysql'
-import config from './config/db-config.json'
-import api from './routes'
-import jwtObj from './config/jwt.json'
-import session from 'express-session'
+import createError from 'http-errors';
+import { createPool } from 'mysql';
+import session from 'express-session';
+import config from './config/db-config.json';
+import api from './routes';
+import jwtObj from './config/jwt.json';
 
-const fileStore = require('session-file-store')(session);
+// const fileStore = require('session-file-store')(session);
 
-//connection SUCCESS CHECK
+// connection SUCCESS CHECK
 const pool = createPool(config);
 
-export const getConnection = (callback) => {
-  pool.getConnection((err, conn) => {
-      if(err) throw err
-      else callback(conn);
-  });
-}
+export const getConnection = callback => {
+    pool.getConnection((err, conn) => {
+        if (err) throw err;
+        else callback(conn);
+    });
+};
 
-const app = express()
+const app = express();
 
 // 세션 설정
-const SECRET_KEY = jwtObj.secret
+const SECRET_KEY = jwtObj.secret;
 const maxAge = 1000 * 60 * 30;
 
-app.use(session({
-  httpOnly: true,	
-  secret: SECRET_KEY,	
-  resave: false,
-  saveUninitialized: true,
-  store: new fileStore(),
-  cookie:{maxAge: maxAge}
-}));
+app.use(
+    session({
+        httpOnly: true,
+        secret: SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+        // store: new fileStore(),
+        cookie: { maxAge },
+    }),
+);
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, '../project_www/build')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../project_www/build')));
 // app.use(cors({ origin: '*'}))
-app.use('/api', api)
+app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
+app.use(function (req, res, next) {
+    next(createError(404));
+});
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500)
-  // res.render('error')
-})
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error')
+});
 
-export default app
+export default app;
--- back/server/routes/group.js
+++ back/server/routes/group.js
@@ -1,348 +1,338 @@
-import { getConnection } from '../app'
 import { Router } from 'express';
-const router = Router()
+import { getConnection } from '../app';
+
+const router = Router();
 
 /*
     그룹 생성하기 : POST /group/create
 */
 router.post('/create', (req, res) => {
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-      return res.status(401).json({
-        code: 20001,
-        msg: "로그인후 사용가능합니다."
-      })
-    }
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
+        }
 
-    /* 그룹 생성 */
-    conn.query(
-      ' INSERT INTO USER_GROUP ' +                    
-      ' (CRT_USER_ID, GROUP_NM, CRT_DTM) ' +
-      '  VALUES (?, ?, SYSDATE()) ' ,
-      [ req.session.user
-      , req.body.GROUP_NM ] ,
-      (err) => {
-        if (err) throw err
-
-        /* 채번 조회 */
+        /* 그룹 생성 */
         conn.query(
-            'SELECT LAST_INSERT_ID() AS LAST_ID',
-            (err2, result2) => {
-				if (err2) throw err2
+            ' INSERT INTO USER_GROUP ' +
+                ' (CRT_USER_ID, GROUP_NM, CRT_DTM) ' +
+                '  VALUES (?, ?, SYSDATE()) ',
+            [req.session.user, req.body.GROUP_NM],
+            err => {
+                if (err) throw err;
 
-                /* 그룹 멤버 추가 */
+                /* 채번 조회 */
                 conn.query(
-                    ' INSERT INTO GROUP_MEMBER ' +                    
-                    ' (GROUP_ID, USER_ID) ' +
-                    '  VALUES (?, ?) ' ,
-                    [ result2[0].LAST_ID
-                    , req.session.user ] ,
-                    (err3) => {
-                        if (err3) throw err3
+                    'SELECT LAST_INSERT_ID() AS LAST_ID',
+                    (err2, result2) => {
+                        if (err2) throw err2;
 
-                        return res.status(200).json({
-                          code: 10000,
-                          msg: "정상 처리되었습니다.",
-                          list:""
-                        })
-                    }
-                )
-            }
-        )
-      }
-    )
-    conn.release()
-  })
-})
+                        /* 그룹 멤버 추가 */
+                        conn.query(
+                            ' INSERT INTO GROUP_MEMBER ' +
+                                ' (GROUP_ID, USER_ID) ' +
+                                '  VALUES (?, ?) ',
+                            [result2[0].LAST_ID, req.session.user],
+                            err3 => {
+                                if (err3) throw err3;
+
+                                return res.status(200).json({
+                                    code: 10000,
+                                    msg: '정상 처리되었습니다.',
+                                    list: '',
+                                });
+                            },
+                        );
+                    },
+                );
+            },
+        );
+        conn.release();
+    });
+});
 
 /*
     그룹 리스트 조회: GET /group/list
 */
 router.get('/list', (req, res) => {
-
-    getConnection((conn) => {
-        if(req.session.user === undefined) {
+    getConnection(conn => {
+        if (req.session.user === undefined) {
             return res.status(401).json({
-              code: 20001,
-              msg: "로그인후 사용가능합니다."
-            })
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
         }
 
         conn.query(
             ' SELECT T02.GROUP_ID                 ' +
-            '      , T02.GROUP_NM                 ' +
-            '   FROM GROUP_MEMBER T01             ' +
-            '      ,   USER_GROUP T02             ' +
-            '  WHERE T01.USER_ID = ?              ' +
-            '    AND T01.GROUP_ID = T02.GROUP_ID  ' ,
-            [ req.session.user ],
+                '      , T02.GROUP_NM                 ' +
+                '   FROM GROUP_MEMBER T01             ' +
+                '      ,   USER_GROUP T02             ' +
+                '  WHERE T01.USER_ID = ?              ' +
+                '    AND T01.GROUP_ID = T02.GROUP_ID  ',
+            [req.session.user],
             (err, result) => {
-                if (err) throw err
+                if (err) throw err;
 
                 return res.status(200).json({
                     code: 10001,
-                    msg: "정상 조회되었습니다.",
-                    list: result
-                })
-            }
-        )
-        conn.release()
-    })
-})
+                    msg: '정상 조회되었습니다.',
+                    list: result,
+                });
+            },
+        );
+        conn.release();
+    });
+});
 
 /*
     그룹에 초대하기 : POST /group/invite
 */
 router.post('/invite', (req, res) => {
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-      return res.status(401).json({
-        code: 20001,
-        msg: "로그인후 사용가능합니다."
-      })
-    }
-
-    conn.query(
-      ' SELECT USER_ID     ' +
-      '   FROM USER        ' +
-      '  WHERE USER_ID = ? ' ,
-      [ req.body.RECV_USER_ID ] ,
-      (err, result) => {
-        if (err) throw err
-
-        if (result.length === 0) {
-          return res.status(400).json({
-            code: 40001,
-            msg: "존재하지 않는 회원입니다.",
-            list: req.body.RECV_USER_ID
-          })
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
         }
 
         conn.query(
-          ' SELECT GROUP_ID     ' +
-          '   FROM GROUP_MEMBER ' +
-          '  WHERE GROUP_ID = ? ' +
-          '    AND USER_ID  = ? ' ,
-          [ req.body.GROUP_ID
-          , req.body.RECV_USER_ID ] ,
-          (err, result) => {
-    
-            if (result.length >= 1) {
-              return res.status(400).json({
-                code: 40002,
-                msg: "그룹 내 이미 사용자가 존재합니다."
-              })
-            }
+            ' SELECT USER_ID     ' +
+                '   FROM USER        ' +
+                '  WHERE USER_ID = ? ',
+            [req.body.RECV_USER_ID],
+            (err, result) => {
+                if (err) throw err;
 
-            conn.query(
-              ' SELECT GROUP_ID     ' +
-              '   FROM GROUP_MEMBER ' +
-              '  WHERE GROUP_ID = ? ' +
-              '    AND USER_ID  = ? ' ,
-              [ req.body.GROUP_ID
-              , req.session.user ] ,
-              (err, result) => {
-                if (err) throw err
-        
                 if (result.length === 0) {
-                  return res.status(401).json({
-                    code: 40000,
-                    msg: "해당 기능의 권한이 없습니다."
-                  })
+                    return res.status(400).json({
+                        code: 40001,
+                        msg: '존재하지 않는 회원입니다.',
+                        list: req.body.RECV_USER_ID,
+                    });
                 }
-        
-                conn.query(
-                  ' INSERT INTO GROUP_INVITE ' +                    
-                  ' (GROUP_ID, SEND_USER_ID, RECV_USER_ID, RES_STATUS, CRT_DTM) ' +
-                  '  VALUES (?, ?, ?, "REQ", SYSDATE()) ' ,
-                  [ req.body.GROUP_ID
-                  , req.session.user
-                  , req.body.RECV_USER_ID ] ,
-                  (err2, result2) => {
-                    if (err2) throw err2
-            
-                    return res.status(200).json({
-                      code: 10000,
-                      msg: "정상 처리되었습니다.",
-                    })
-                  }
-                )
-              }
-            )
-          }
-        )
-      }
-    )
 
-    conn.release()
-  }) 
-})
+                conn.query(
+                    ' SELECT GROUP_ID     ' +
+                        '   FROM GROUP_MEMBER ' +
+                        '  WHERE GROUP_ID = ? ' +
+                        '    AND USER_ID  = ? ',
+                    [req.body.GROUP_ID, req.body.RECV_USER_ID],
+                    (err2, result2) => {
+                        if (err2) throw err2;
+                        if (result2.length >= 1) {
+                            return res.status(400).json({
+                                code: 40002,
+                                msg: '그룹 내 이미 사용자가 존재합니다.',
+                            });
+                        }
+
+                        conn.query(
+                            ' SELECT GROUP_ID     ' +
+                                '   FROM GROUP_MEMBER ' +
+                                '  WHERE GROUP_ID = ? ' +
+                                '    AND USER_ID  = ? ',
+                            [req.body.GROUP_ID, req.session.user],
+                            (err3, result3) => {
+                                if (err3) throw err3;
+
+                                if (result3.length === 0) {
+                                    return res.status(401).json({
+                                        code: 40000,
+                                        msg: '해당 기능의 권한이 없습니다.',
+                                    });
+                                }
+
+                                conn.query(
+                                    ' INSERT INTO GROUP_INVITE ' +
+                                        ' (GROUP_ID, SEND_USER_ID, RECV_USER_ID, RES_STATUS, CRT_DTM) ' +
+                                        '  VALUES (?, ?, ?, "REQ", SYSDATE()) ',
+                                    [
+                                        req.body.GROUP_ID,
+                                        req.session.user,
+                                        req.body.RECV_USER_ID,
+                                    ],
+                                    err4 => {
+                                        if (err4) throw err4;
+
+                                        return res.status(200).json({
+                                            code: 10000,
+                                            msg: '정상 처리되었습니다.',
+                                        });
+                                    },
+                                );
+                            },
+                        );
+                    },
+                );
+            },
+        );
+
+        conn.release();
+    });
+});
 
 /*
     초대를 처리하지 않은 수 조회 : GET /group/invite/not-proc
 */
 router.get('/invite/not-proc', (req, res) => {
-
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-        return res.status(401).json({
-          code: 20001,
-          msg: "로그인후 사용가능합니다."
-        })
-    }
-
-    conn.query(
-        ' SELECT COUNT(*) AS count       ' +
-        '   FROM GROUP_INVITE T01        ' +
-        '  WHERE T01.RECV_USER_ID = ?    ' +
-        '    AND T01.RES_STATUS = "REQ"  ' ,
-        [ req.session.user ],
-        (err, result) => {
-            if (err) throw err
-
-            return res.status(200).json({
-              success: true,
-              result: result
-            })
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
         }
-    )
-    conn.release()
-  })
-})
+
+        conn.query(
+            ' SELECT COUNT(*) AS count       ' +
+                '   FROM GROUP_INVITE T01        ' +
+                '  WHERE T01.RECV_USER_ID = ?    ' +
+                '    AND T01.RES_STATUS = "REQ"  ',
+            [req.session.user],
+            (err, result) => {
+                if (err) throw err;
+
+                return res.status(200).json({
+                    success: true,
+                    result,
+                });
+            },
+        );
+        conn.release();
+    });
+});
 
 /*
     그룹 초대 리스트 조회 : GET /group/invite/list
 */
 router.get('/invite/list', (req, res) => {
-
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-        return res.status(401).json({
-          code: 20001,
-          msg: "로그인후 사용가능합니다."
-        })
-    }
-
-    conn.query(
-        ' SELECT T01.INVITE_ID                ' +
-        '      , T01.GROUP_ID                 ' +
-        '      , T02.GROUP_NM                 ' +
-        '      , T01.SEND_USER_ID             ' +
-        '      , T01.RECV_USER_ID             ' +
-        '      , T01.RES_STATUS               ' +
-        '   FROM GROUP_INVITE T01             ' +
-        '      , USER_GROUP T02               ' +
-        '  WHERE T01.RECV_USER_ID = ?         ' +
-        '    AND T01.GROUP_ID = T02.GROUP_ID  ' ,
-        [ req.session.user ],
-        (err, result) => {
-            if (err) throw err
-
-            return res.status(200).json({
-                success: true,
-                result: result
-            })
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
         }
-    )
-    conn.release()
-  })
-})
+
+        conn.query(
+            ' SELECT T01.INVITE_ID                ' +
+                '      , T01.GROUP_ID                 ' +
+                '      , T02.GROUP_NM                 ' +
+                '      , T01.SEND_USER_ID             ' +
+                '      , T01.RECV_USER_ID             ' +
+                '      , T01.RES_STATUS               ' +
+                '   FROM GROUP_INVITE T01             ' +
+                '      , USER_GROUP T02               ' +
+                '  WHERE T01.RECV_USER_ID = ?         ' +
+                '    AND T01.GROUP_ID = T02.GROUP_ID  ',
+            [req.session.user],
+            (err, result) => {
+                if (err) throw err;
+
+                return res.status(200).json({
+                    success: true,
+                    result,
+                });
+            },
+        );
+        conn.release();
+    });
+});
 
 /*
     그룹 초대 수락 : POST /group/invite/accept
 */
 router.post('/invite/accept', (req, res) => {
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
+        }
 
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-        return res.status(401).json({
-          code: 20001,
-          msg: "로그인후 사용가능합니다."
-        })
-    }
-
-    /* 유효한 초대내역인지 체크 */
-    conn.query(
-      ' SELECT GROUP_ID                 ' +
-      '   FROM GROUP_INVITE T01         ' +
-      '  WHERE T01.INVITE_ID    = ?     ' +
-      '    AND T01.RECV_USER_ID = ?     ' +
-      '    AND T01.RES_STATUS   = "REQ" ' ,
-      [ req.body.INVITE_ID
-      , req.session.user ] ,
-      (err, result) => {
-          if (err) throw err
-
-          const groupId = result[0].GROUP_ID
-
-          /* 그룹 멤버 테이블에 등록 */
-          conn.query(
-            ' INSERT INTO GROUP_MEMBER ' +                    
-            ' (GROUP_ID, USER_ID)      ' +
-            '  VALUES (?, ?) ' ,
-            [ groupId
-            , req.session.user ] ,
+        /* 유효한 초대내역인지 체크 */
+        conn.query(
+            ' SELECT GROUP_ID                 ' +
+                '   FROM GROUP_INVITE T01         ' +
+                '  WHERE T01.INVITE_ID    = ?     ' +
+                '    AND T01.RECV_USER_ID = ?     ' +
+                '    AND T01.RES_STATUS   = "REQ" ',
+            [req.body.INVITE_ID, req.session.user],
             (err, result) => {
-              if (err) throw err
+                if (err) throw err;
 
-              /* 초대 내역 상태 변경 */
-              conn.query(
-                  ' UPDATE GROUP_INVITE T01             ' +
-                  '    SET T01.RES_STATUS   = "ACP"     ' +
-                  '      , T01.RES_DTM      = SYSDATE() ' +
-                  '  WHERE T01.INVITE_ID    = ?         ' +
-                  '    AND T01.RECV_USER_ID = ?         ' +
-                  '    AND T01.RES_STATUS   = "REQ"     ' ,
-                  [ req.body.INVITE_ID
-                  , req.session.user ],
-                  (err, result) => {
-                      if (err) throw err
-          
-                      return res.status(200).json({
-                          success: true
-                      })
-                  }
-              )
-            }
-          )
-      }
-    )
-    conn.release()
-  })
-})
+                const groupId = result[0].GROUP_ID;
+
+                /* 그룹 멤버 테이블에 등록 */
+                conn.query(
+                    ' INSERT INTO GROUP_MEMBER ' +
+                        ' (GROUP_ID, USER_ID)      ' +
+                        '  VALUES (?, ?) ',
+                    [groupId, req.session.user],
+                    err2 => {
+                        if (err2) throw err2;
+
+                        /* 초대 내역 상태 변경 */
+                        conn.query(
+                            ' UPDATE GROUP_INVITE T01             ' +
+                                '    SET T01.RES_STATUS   = "ACP"     ' +
+                                '      , T01.RES_DTM      = SYSDATE() ' +
+                                '  WHERE T01.INVITE_ID    = ?         ' +
+                                '    AND T01.RECV_USER_ID = ?         ' +
+                                '    AND T01.RES_STATUS   = "REQ"     ',
+                            [req.body.INVITE_ID, req.session.user],
+                            err3 => {
+                                if (err3) throw err3;
+
+                                return res.status(200).json({
+                                    success: true,
+                                });
+                            },
+                        );
+                    },
+                );
+            },
+        );
+        conn.release();
+    });
+});
 
 /*
     그룹 초대 거절 : POST /group/invite/not-accept
 */
 router.post('/invite/not-accept', (req, res) => {
-
-  getConnection((conn) => {
-    if(req.session.user === undefined) {
-        return res.status(401).json({
-          code: 20001,
-          msg: "로그인후 사용가능합니다."
-        })
-    }
-
-    conn.query(
-        ' UPDATE GROUP_INVITE T01             ' +
-        '    SET T01.RES_STATUS   = "REJ"     ' +
-        '      , T01.RES_DTM      = SYSDATE() ' +
-        '  WHERE T01.INVITE_ID    = ?         ' +
-        '    AND T01.RECV_USER_ID = ?         ' +
-        '    AND T01.RES_STATUS   = "REQ"     ' ,
-        [ req.body.INVITE_ID
-        , req.session.user ],
-        (err, result) => {
-            if (err) throw err
-
-            return res.status(200).json({
-                success: true
-            })
+    getConnection(conn => {
+        if (req.session.user === undefined) {
+            return res.status(401).json({
+                code: 20001,
+                msg: '로그인후 사용가능합니다.',
+            });
         }
-    )
-    conn.release()
-  })
-})
 
-export default router
+        conn.query(
+            ' UPDATE GROUP_INVITE T01             ' +
+                '    SET T01.RES_STATUS   = "REJ"     ' +
+                '      , T01.RES_DTM      = SYSDATE() ' +
+                '  WHERE T01.INVITE_ID    = ?         ' +
+                '    AND T01.RECV_USER_ID = ?         ' +
+                '    AND T01.RES_STATUS   = "REQ"     ',
+            [req.body.INVITE_ID, req.session.user],
+            err => {
+                if (err) throw err;
+
+                return res.status(200).json({
+                    success: true,
+                });
+            },
+        );
+        conn.release();
+    });
+});
+
+export default router;
