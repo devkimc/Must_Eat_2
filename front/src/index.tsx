7b868765c0882335f9553e350ddf39c75e45635c
franco <kws60000@naver.com>
2022-08-13 14:00:10 +0900

    style: style 적용 분리 (globalStyle, reset)

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,50 +1,13 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { ToastContainer } from 'react-toastify';
 
+import GlobalStyle from 'style/GlobalStyle';
 import App from './App';
-import 'assets/css/index.css';
-import 'assets/css/reset.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'GangwonEdu_OTFBoldA';
-      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/GangwonEdu_OTFBoldA.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-  }
-
-  @font-face {
-      font-family: 'LeferiPoint-WhiteObliqueA';
-      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-WhiteObliqueA.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-  }
-  
-  body {
-    font-family: 'GangwonEdu_OTFBoldA';
-  }
-  
-  a {
-    text-decoration: none;
-  }
-
-  input {
-    border: none;
-    outline: none;
-  }
-  
-  button {
-    outline: none;
-    background-color: transparent;
-    cursor: pointer;
-  }
-`;
 
 root.render(
     <RecoilRoot>
